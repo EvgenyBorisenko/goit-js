c937bf96d72dd0d74dc72758cd22de5f671ff3b4
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-08-12 11:13:25 +0300

    hw 12 done

--- HomeWork-12/src/index.js
+++ HomeWork-12/src/index.js
@@ -7,8 +7,11 @@ const serchForm = document.getElementById('search-form');
 serchForm.addEventListener('submit', e => {
   e.preventDefault();
   console.log('e.currentTarget.elements.city.value :', e.currentTarget.elements.city.value);
-  // fetchWeather => data
-  // render(data)
+  fetchWeather(e.currentTarget.elements.city.value)
+  .then(data => {
+    console.log('fetch', data);
+    render(data)
+  });
 });
 
 
@@ -17,16 +20,16 @@ getGeoPosition().then(data => {
   fetchWeather(data)
   .then(data => {
     console.log('fetch', data);
-    // render(data)
-    document.querySelector('[data-field="conditions"]').textContent = data.current.condition.text;
-    document.querySelector('.icon').textContent = data.current.condition.icon;
-    document.querySelector('[data-field="wind"]').textContent = `${data.current.wind_kph}kph`;
-    document.querySelector('[data-field="location"]').textContent = data.location.name;
-    document.querySelector('[data-field="temp"]').textContent = `${data.current.temp_c }\u2103 `;
-    document.querySelector('[data-field="humidity"]').textContent =`${ data.current.humidity }%`;
+    render(data)
   });
 })
 
-function render(fetchData) {
+function render(data) {
+  document.querySelector('[data-field="conditions"]').textContent = data.current.condition.text;
+  document.querySelector('.icon').textContent = data.current.condition.icon;
+  document.querySelector('[data-field="wind"]').textContent = `${data.current.wind_kph}kph`;
+  document.querySelector('[data-field="location"]').textContent = data.location.name;
+  document.querySelector('[data-field="temp"]').textContent = `${data.current.temp_c }\u2103 `;
+  document.querySelector('[data-field="humidity"]').textContent =`${ data.current.humidity }%`;
   
 }
