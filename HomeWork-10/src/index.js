64e0f90fcf588bf3ef60d9e48300ef7e8da40f2e
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-08-13 10:18:21 +0300

    changed HW-10

--- HomeWork-10/src/index.js
+++ HomeWork-10/src/index.js
@@ -3,7 +3,13 @@ import menuJson from './menu.json';
 import menuTemplaite from './templates/template.hbs';
 
 const restoranMenu = document.querySelector('.menu');
-const body = document.body;
+const toggleSwitch = document.getElementById('theme-switch-control');
+const body = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 let markup = menuJson
   .map(el => {
@@ -13,17 +19,30 @@ let markup = menuJson
 
 restoranMenu.insertAdjacentHTML('afterbegin', markup);
 
-const toggleSwitch = document.getElementById('theme-switch-control');
-const currentTheme = localStorage.getItem('theme');
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+if (localStorage.getItem('theme') !== null) {
+  const currentTheme = localStorage.getItem('theme');
+  body.classList.add(currentTheme);
+  if (currentTheme === Theme.DARK) {
+    toggleSwitch.checked = true;
+  } else {
+    toggleSwitch.checked = false;
+  }
+} else {
+  localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+}
 
 toggleSwitch.addEventListener('change', function(ev) {
+
   if (body.classList.contains(Theme.DARK)) {
+    console.log('body.classList.contains(Theme.DARK) :', body.classList.contains(Theme.DARK));
+    console.dir(body);
     body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+    toggleSwitch.checked = false;
   } else {
+    body.classList.remove(Theme.LIGHT);
     body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+    toggleSwitch.checked = true;
   }
 });
